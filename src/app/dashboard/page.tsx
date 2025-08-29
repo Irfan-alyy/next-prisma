3bcd34b97a2cab0dfd38b05335643252ced9154a
Ali Irfan <irfanserboon562@gmail.com>
2025-08-29 21:02:04 +0500

    fix: show jobs and applications tabs authenticated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -251,7 +251,7 @@ const DashboardPage: React.FC = () => {
               >
                 Profile
               </button>
-              <button
+             { session?.user?.type=="employer" && <button
                 onClick={() => handleTabChange("job")}
                 className={`px-6 py-3 text-base font-medium rounded-md shadow-sm transition duration-200 ${activeTab === 'jobs'
                   ? 'bg-indigo-600 text-white'
@@ -259,8 +259,8 @@ const DashboardPage: React.FC = () => {
                   }`}
               >
                 My Jobs
-              </button>
-              <button
+              </button>}
+             {session?.user?.type=="candidate" && <button
                 onClick={() => handleTabChange("application")}
                 className={`px-6 py-3 text-base font-medium rounded-md shadow-sm transition duration-200 ${activeTab === 'applications'
                   ? 'bg-indigo-600 text-white'
@@ -268,7 +268,7 @@ const DashboardPage: React.FC = () => {
                   }`}
               >
                 My Applications
-              </button>
+              </button>}
             </div>
 
             <motion.div
